9ab60cb6ba100123ad2f33fba345764ae4ebd54c
Ulisse Mini <ulisse.mini@gmail.com>
2021-04-26 16:15:20 -0400

    Add graphs

--- graphs/graphs.py
+++ graphs/graphs.py
@@ -9,6 +9,23 @@ def K(n):
     return graph
 
 
+def undirected(g):
+    "Turn a directed graph into an undirected graph"
+
+    g = g.copy()
+    for node, neighbors in enumerate(g):
+        for neighbor in neighbors:
+            # (node, neighbor) edge exists add edge (neighbor, node)
+            # if needed
+            if node not in g[neighbor]:
+                g[neighbor].append(node)
+
+    return g
+
 
 G1 = [[2,4,5], [4,5], [3,4], [], [5], []]
+
+T1 = [[2], [4], [3,4], [5], [], []] # 1 center
+T2 = [[], [0,3], [3], [], [1], [4], [3], [3], [4], [6]] # 2 centers
+
 
